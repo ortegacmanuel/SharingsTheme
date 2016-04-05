e01c08f954d684c6ea8be8846abace02530c317f
Manuel Ortega <manuel@enkidu.coop>
2016-04-05 13:14:08 +0200

    Primera versión de la integración del formulario para agregar un nuevo objeto o servicio con el tema

--- actions/sharingsthemenew.php
+++ actions/sharingsthemenew.php
@@ -11,5 +11,78 @@ class SharingsThemeNewAction extends NewSharingsAction {
         SharingsThemeBody::showBody($this);
     }
 
+    function showContent()
+    {
+        if (!empty($this->error)) {
+            $this->element('p', 'error', $this->error);
+        }
+
+        $this->elementStart('div', array('class' => 'container main-container'));
+
+
+        $this->elementStart('div', array('class' => 'row'));
+
+        $this->elementStart('div', array('class' => 'breadcrumbDiv col-lg-12'));
+
+        $this->elementStart('ul', array('class' => 'breadcrumb'));
+
+            $this->elementStart('li');
+            $this->elementStart('a', array('href' => '#'));
+            $this->raw('Inicio');
+            $this->elementEnd('a');
+            $this->elementEnd('li');
+
+            $this->elementStart('li');
+            $this->elementStart('a', array('href' => '#'));
+            $this->raw('Sharings');
+            $this->elementEnd('a');
+            $this->elementEnd('li');
+
+            $this->elementStart('li');
+            $this->elementStart('a', array('href' => '#'));
+            $this->raw('Nuevo');
+            $this->elementEnd('a');
+            $this->elementEnd('li');
+
+        $this->elementEnd('ul');
+
+        $this->elementEnd('div');
+        $this->elementEnd('div');
+
+        $this->elementStart('div', array('class' => 'row'));
+
+        $this->elementStart('div', array('class' => 'col-lg-9 col-md-9 col-sm-7 col-xs-6 col-xxs-12 text-center-xs'));
+
+        $this->elementStart('h1', array('class' => 'section-title-inner'));
+        $this->elementStart('span');
+
+        $this->elementStart('i', array('class' => 'glyphicon glyphicon-plus'));
+        $this->elementEnd('i');
+        $this->raw('Nuevo objeto o servicio');
+        $this->elementEnd('span');
+        $this->elementEnd('h1');
+
+        $this->elementEnd('div');
+
+        $form = new NewSharingsForm($this,
+                                 $this->displayName,
+                                 $this->summary, 
+                                 $this->price, 
+                                 $this->sharing_category_id,
+                                 $this->sharing_type_id,
+                                 $this->sharing_city_id);
+
+        $form->show();
+
+        $this->elementStart('div', array('class' => 'gap'));
+        $this->elementEnd('div');
+
+        $this->elementEnd('div');
+        $this->elementEnd('div');
+
+        $this->elementEnd('div');
+
+        return;
+    }
 
 }
